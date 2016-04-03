bba5813b45de5cbe230be16bf360fa95b492b8af
gohv <hristovsotirovgeorgi@gmail.com>
2016-04-03 15:32:35 +0300

    Update test.java

--- src/test.java
+++ src/test.java
@@ -15,15 +15,15 @@ class Test
     System.out.println();
   }
 
-  // Implementing Fisher–Yates shuffle
+  // Fisherâ€“Yates shuffle
   static void shuffleArray(int[] ar)
   {
-    // If running on Java 6 or older, use `new Random()` on RHS here
+   
     Random rnd = ThreadLocalRandom.current();
     for (int i = ar.length - 1; i > 0; i--)
     {
       int index = rnd.nextInt(i + 1);
-      // Simple swap
+      // swap
       int a = ar[index];
       ar[index] = ar[i];
       ar[i] = a;
